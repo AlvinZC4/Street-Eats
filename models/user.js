bce62e061deb6c9edb9e4a344ca7b5643d0a3606
Alvin <alvinzcoxiv@gmail.com>
2021-02-15 22:31:01 -0500

    completed model for user

--- models/user.js
+++ models/user.js
@@ -5,6 +5,18 @@ module.exports = function(sequelize, Datatypes) {
             allowNull: false,
             unique: true
         },
+        email: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
         first_name: {
             type: Datatypes.STRING,
             allowNull: false
